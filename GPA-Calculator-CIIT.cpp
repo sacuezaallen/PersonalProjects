b85a1259c68a34c41bf08bc49a9acd06ffc901f6
allensacueza <allen.sacueza@ciit.edu.ph>
2026-03-31 15:50:45 +0800

    Completed Menu Commands

--- GPA-Calculator-CIIT.cpp
+++ GPA-Calculator-CIIT.cpp
@@ -1,27 +1,37 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <thread>
+#include <chrono>
 using namespace std;
 
 int choice; // User's choice in the menu (u‿ฺu✿ฺ)
 
 string SubjectName;
-int GradeFS; // Grade For Subject
+float GradeFS; // Grade For Subject
 int UnitFS; // Amount of units for that subject
 string Name;
 
+float totalGPA;
+float finalGPA;
+int totalUnits;
+
 int Empty = 0;
 
-int Grades[15];
+float Grades[15] = {0};
 int Units[15];
+float GPA[15] = {0};
 string Subjects[15];
-string StudentNames [5];
-int CGrades[5]; // Calculated Grades
 
 // declaration of functions vv o(=•ェ•=)o ``~!
 void Menu(); 
 void NewGrade();
+void CalcGPA();
+void displayALL();
+void restart();
+
 int emptyGrade();
+int emptyIndex();
 
 int main()
     {
@@ -38,18 +48,36 @@ int main()
             {
                 NewGrade();
             }
-            else
+            else if (choice == 2)
+            {
+                CalcGPA();
+            }
+            else if (choice == 3)
+            {
+                displayALL();
+            }
+            else if (choice == 4)
+            {
+                Grades[15] = {0};
+            }
+            else if (choice <= 0 || choice >= 6)
             {
                 cout << "Invalid. Choice is not within the answers available.\n";
                 cout << "Please enter a new number from the provided.\n";
             }
 
-        } while (choice != 4);
+        } while (choice != 5);
+
+        cout << "\nExiting program...";
+        this_thread::sleep_for(500ms);
+        cout << "\n...";
+        this_thread::sleep_for(500ms);
+        cout << "Thank you!";
 
         return 0;
     }
 
-void Menu() // Reusable code for MENU
+void Menu()
 {
         cout << "\n=======================\n";
         cout << "   GPA CALCULATOR\n";
@@ -76,36 +104,75 @@ void NewGrade()
     else
     {
         cout << "Enter NAME of subject: ";
-        cin >> SubjectName;
-            cin.ignore();
+        cin.ignore();
+        getline(cin, SubjectName);
         cout << "Enter GRADE for that subject: ";
         cin >> GradeFS;
         cout << "Enter amount of UNITS for that subject: ";
         cin >> UnitFS;
 
-        for (int i = 0; i < 15; i++) // check for nearest empty index
+        Empty = 0;
+
+        for (int i = 0; i < 15; i++) 
             {
-                if (Grades[i] == 0)
+                if (Grades[i] == 0 && Empty == 0)
                 {
                     Subjects[i] = SubjectName;
                     Grades[i] = GradeFS;
                     Units[i] = UnitFS;
+
+                    Empty++;
                 }
             }
+
+            Empty = 0;
 
         cout << "\n\nGrades successfully added!";
     }
 }
 
-void displayALL()
+void CalcGPA()
 {
     for (int i = 0; i < 15; i++)
     {
-        //wait lang tinatamad na ako
+        if (Grades[i] != 0)
+        {
+            GPA[i] = Grades[i] * Units[i];
+
+            totalUnits += Units[i];
+        }
+
+        totalGPA += GPA[i];
     }
+
+    finalGPA = totalGPA / totalUnits;
+
+    cout << "\nYour final GPA is: " << finalGPA;
 }
 
-int emptyGrade()
+void displayALL()
+{
+    emptyIndex();
+
+    cout << left << setw(15) << "Subject"
+         << left << setw(5) << "Grade"
+         << right << setw(10) << "Units\n"
+         << "===================================\n";
+
+    for (int i = 0; i < 15; i++)
+    {
+        if (Grades[i] != 0)
+        {
+            cout << left << setw(15) << Subjects[i]
+                 << left << setw(5) << Grades[i]
+                 << right << setw(10) << Units[i] << endl;
+        }
+    }
+
+    Empty = 0;
+}
+
+int emptyGrade() //checks if array has empty indexes
 {
     for (int i = 0; i < 15; i++) // check if array is full
     {
@@ -117,3 +184,19 @@ int emptyGrade()
     
     return Empty;
 }
+
+int emptyIndex()
+{
+    // find nearest empty index in the array
+
+    for (int i = 0; i < 15; i++)
+    {
+        if (Grades[i] != 0)
+        {
+            Empty++;
+        }
+    }
+
+    return Empty;
+}
+
